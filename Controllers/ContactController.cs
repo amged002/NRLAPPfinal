f310eeebe10f7f4f3ad9233d0e7a20282c2b9376
Storm300 <164446393+Storm300@users.noreply.github.com>
2025-11-23 18:44:42 +0100

    La til kommentarer i ContactController

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -2,8 +2,14 @@
 
 namespace NRLApp.Controllers
 {
+/// <summary>
+/// Controller til kontaktsiden.
+/// <summary>
     public class ContactController : Controller
     {
+    /// <summary>
+    /// Returnerer kontaktsiden uten modell-data
+    /// <summary>
         public IActionResult Index()
         {
             return View();
